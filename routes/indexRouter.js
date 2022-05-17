41c041e6b869f5d5ae758f8c2d4cc33769018cae
Saaba <nathan.albino01@gmail.com>
2022-05-17 03:06:24 -0400

    added partials/navbar

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("YER");
+  res.render("index");
 });
 
 export { router as indexRouter };
